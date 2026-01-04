6ba084a1ed66152fecf4a2c039673fa99891df27
Julia Baucher <juliabaucher.work@gmail.com>
2026-01-04 17:32:35 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/julia-s-ai-assistant/",
+  base: "/lovable-aws-bedrock-chatbot/",
   server: {
     host: "::",
     port: 8080,
